a99469ad321d4b26fc323e2a19469eddd6b34ab3
Ziad Abdalla <105611439+ziad223@users.noreply.github.com>
2024-04-12 19:33:37 +0200

    Add files via upload

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,11 @@
+import './banner.css';
+const Banner = () => {
+  return (
+    <div className='banner container'>
+      <div className="banner-left"></div>
+      <div className="banner-right"> </div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/pages/services/Services.jsx
@@ -0,0 +1,11 @@
+import './services.css';
+import Service from '../../components/service/Service';;
+const Services = () => {
+  return (
+    <div>
+      <Service/>
+    </div>
+  )
+}
+
+export default Services
